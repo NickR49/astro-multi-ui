22b6ca3990f9b2242f9461d1b8921ee257caeef9
Nick Rountree <nick@csvue.com>
2023-03-04 22:03:23 +1300

    Improve layout

--- src/components/react/Header.tsx
+++ src/components/react/Header.tsx
@@ -1,5 +1,14 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { open } from "../../leftNavStore";
+import { useStore } from "@nanostores/react";
 
 interface Props {
   onClick: () => void;
@@ -8,13 +17,23 @@ interface Props {
 export default function Header(props: Props) {
   const { onClick } = props;
 
+  const $open = useStore(open);
+
+  const theme = useTheme();
+  const smallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
-    <AppBar position="relative">
+    <AppBar
+      position="fixed"
+      sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+    >
       <Toolbar disableGutters sx={{ ml: 0.75, mr: 2 }}>
-        <IconButton onClick={onClick}>
-          <MenuIcon />
-        </IconButton>
-        <Typography>N I X</Typography>
+        {smallScreen && (
+          <IconButton onClick={() => open.set(!$open)}>
+            <MenuIcon />
+          </IconButton>
+        )}
+        <Typography ml={smallScreen ? 1 : 2}>N I X</Typography>
       </Toolbar>
     </AppBar>
   );
--- src/components/react/LeftNav.tsx
+++ src/components/react/LeftNav.tsx
@@ -4,8 +4,14 @@ import {
   ListItem,
   ListItemButton,
   ListItemText,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
+import { useStore } from "@nanostores/react";
 import type { CollectionEntry } from "astro:content";
+import { open } from "../../leftNavStore";
 
 interface Props {
   items: CollectionEntry<"example">[];
@@ -14,9 +20,22 @@ interface Props {
 export default function LeftNav(props: Props) {
   const { items } = props;
 
+  const $open = useStore(open);
+
+  const theme = useTheme();
+  const smallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
-    <Drawer variant="permanent" open>
+    <Drawer
+      variant={smallScreen ? "temporary" : "permanent"}
+      open={$open}
+      sx={{ top: 30 }}
+    >
+      <Toolbar />
       <List>
+        <Typography m={1} fontWeight={600}>
+          Astro Examples
+        </Typography>
         {items.map((item, index) => (
           <ListItem key={item.id} disablePadding sx={{ display: "block" }}>
             <ListItemButton
@@ -24,7 +43,12 @@ export default function LeftNav(props: Props) {
                 minHeight: 48,
                 px: 2.5,
               }}
+              onClick={() => {
+                console.log(`Setting open to false`);
+                open.set(false);
+              }}
             >
+              {/* TODO We need to use routing to avoid a full page refresh */}
               <ListItemText
                 primary={
                   <a href={`/examples/${item.slug}`}>{item.data.title}</a>
